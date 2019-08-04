534e4f4ad28ffcfef7654178f6e63c90f7e27c38
Evgheni Curdov <kurdov.e@gmail.com>
2019-08-04 12:20:16 -0700

    randomized initial grid done

--- src/App.js
+++ src/App.js
@@ -11,8 +11,26 @@ class App extends Component {
       {shape: "https://img.icons8.com/cotton/64/000000/owl--v1.png"},
       {shape: "https://img.icons8.com/cotton/64/000000/bulldog--v1.png"},
       {shape: "https://img.icons8.com/cotton/64/000000/crab--v1.png"},
-      {shape: "https://img.icons8.com/doodle/48/000000/turkeycock.png"}
+      {shape: "https://img.icons8.com/doodle/48/000000/turkeycock.png"},
+      {shape: "https://img.icons8.com/doodle/96/000000/hatching-chicken.png"},
+      {shape: "https://img.icons8.com/doodle/96/000000/hornet-hive.png"},
+      {shape: "https://img.icons8.com/doodle/48/000000/sea-shell.png"},
+      {shape: "https://img.icons8.com/nolan/96/000000/unicorn.png"},
+      {shape: "https://img.icons8.com/nolan/64/000000/orca.png"},
+      {shape: "https://img.icons8.com/plasticine/100/000000/bird.png"},
+      {shape: "https://img.icons8.com/plasticine/100/000000/pig.png"},
     ]
+  }
+
+  randomizer = () => {
+    var icon = this.state.icons[Math.floor((Math.random() * 7) + 1)].shape;
+    console.log (typeof icon);
+    // return icon;
+    // // let num = this.state.icons.length;
+    // var num = Math.floor((Math.random() * 7) + 1);
+    // console.log (num);
+    // console.log ('2nd log: ', this.state.icons[num].shape);
+    // // return num;
   }
 
   updateGrid = () => {
@@ -23,28 +41,34 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Welcome To Bejeweled</h1>
-        <p>Challenge yorself !</p>
+        <h3>Challenge yorself !</h3>
         <hr />
 
         <div className="Grid-container">
-          <div><Cell icon={this.state.icons[0].shape}/></div>
-          <div><Cell icon={this.state.icons[2].shape}/></div>
-          <div><Cell icon={this.state.icons[1].shape}/></div>
-          <div><Cell icon={this.state.icons[6].shape}/></div>
-          <div><Cell icon={this.state.icons[3].shape}/></div>
-          <div><Cell icon={this.state.icons[5].shape}/></div>
-          <div><Cell icon={this.state.icons[4].shape}/></div>
+          {/* {this.state.icons.map(symbol => {
+            return <div><Cell icon={symbol.shape}/></div>
+          })} */}
 
-          <div><Cell /></div>
-          <div><Cell /></div>
-          <div><Cell /></div>
-          <div><Cell /></div>
-          <div><Cell /></div>
-          <div><Cell /></div>
-          <div><Cell /></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
+          <div><Cell icon={this.state.icons[Math.floor((Math.random() * 7) + 1)].shape}/></div>
 
         </div>
-        <button onClick={this.updateGrid}>Update Grid</button>
+        <button onClick={this.randomizer}>Update Grid</button>
 
       </div>
     );
